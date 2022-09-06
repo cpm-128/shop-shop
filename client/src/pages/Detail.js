557cadc64d22f6fa62794023c0cfddbe5b013de4
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-06 11:18:12 -0400

    rendering Shopping Cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
+import Cart from '../components/Cart';
+
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
@@ -31,6 +33,7 @@ function Detail() {
 
   return (
     <>
+
       {currentProduct ? (
         <div className="container my-1">
           <Link to="/">← Back to Products</Link>
@@ -52,6 +55,9 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+
+      <Cart />
+
     </>
   );
 }
