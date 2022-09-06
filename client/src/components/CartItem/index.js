20212b291c1afa3b2cd3622a65a18c3557750639
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-06 17:54:25 -0400

    delete items from cart updates idb

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
+import { idbPromise } from '../../utils/helpers';
+
 // expects an 'item' object as a prop
 const CartItem = ({ item }) => {
 
@@ -12,6 +14,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', {...item});
     };
 
     // manually enter quant value in cart
@@ -23,12 +26,14 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             });
+            idbPromise('cart', 'delete', {...item});
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             });
+            idbPromise('cart', 'put', {...item, purchaseQuantity: parseInt(value) });
         }
     };
 
