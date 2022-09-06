addb27258dd41bcbfadb4abd6a8a1b3dc4c497e4
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-06 12:06:10 -0400

    add items to cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+
 function ProductItem(item) {
+
+  // define state variable, dispatch function, and function that will call the add_to_cart action
+  const [state, dispatch] = useStoreContext();
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...item, purchaseQuantity: 1 }
+    });
+  };
+
+  // define item
   const {
     image,
     name,
@@ -24,7 +38,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
